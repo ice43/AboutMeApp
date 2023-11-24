2c0e2a7a5947fb7d6abb623b8c96eb2fae29c564
bowski43 <43mngt@gmail.com>
2023-11-24 12:26:28 +0300

    business logic login vc

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
+    
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+      
     }
-
-
+    
+    
+    @IBAction private func forgotUsernameAction() {
+        let alert = UIAlertController(
+            title: "Oops!",
+            message: "Your name is User 😉",
+            preferredStyle: .alert
+        )
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okButton)
+        present(alert, animated: true)
+    }
+    
+    @IBAction private func forgotPasswordAction() {
+        let alert = UIAlertController(
+            title: "Oops!",
+            message: "Your password is Password 😉",
+            preferredStyle: .alert
+        )
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okButton)
+        present(alert, animated: true)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
+
+
+
+
 
