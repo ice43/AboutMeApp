adb4a32ee6b8a0fc698fc4980924b9ff78aad2bd
bowski43 <43mngt@gmail.com>
2023-11-24 14:24:55 +0300

    added some functional for login vc

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -9,20 +9,45 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
+    // MARK: - IB Outlets
     @IBOutlet private weak var usernameTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-      
+    
+    // MARK: - Private Properties
+    private let username = "user"
+    private let password = "pass"
+    
+    // MARK: - Override Methods
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
+    // MARK: - Navigation
+    override func shouldPerformSegue(
+        withIdentifier identifier: String,
+        sender: Any?
+    ) -> Bool {
+        guard usernameTextField.text == username,
+              passwordTextField.text == password else {
+            invalidData()
+            return false
+        }
+        
+        return true
+    }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else {
+            return
+        }
+        welcomeVC.welcomeText = "Welcome, \(usernameTextField.text ?? "")!"
+    }
+    
+    // MARK: - IB Actions
     @IBAction private func forgotUsernameAction() {
         let alert = UIAlertController(
             title: "Oops!",
-            message: "Your name is User 😉",
+            message: "Your name is \(username) 😉",
             preferredStyle: .alert
         )
         let okButton = UIAlertAction(title: "OK", style: .default)
@@ -34,7 +59,7 @@ final class LoginViewController: UIViewController {
     @IBAction private func forgotPasswordAction() {
         let alert = UIAlertController(
             title: "Oops!",
-            message: "Your password is Password 😉",
+            message: "Your password is \(password) 😉",
             preferredStyle: .alert
         )
         let okButton = UIAlertAction(title: "OK", style: .default)
@@ -43,8 +68,22 @@ final class LoginViewController: UIViewController {
         present(alert, animated: true)
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+    @IBAction private func unwind(for segue: UIStoryboardSegue) {
+        passwordTextField.text = ""
+        usernameTextField.text = ""
+    }
+    
+    // MARK: - Private Methods
+    private func invalidData() {
+        let alert = UIAlertController(
+            title: "Invalid login or password",
+            message: "Please, enter correct login and password",
+            preferredStyle: .alert
+        )
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okButton)
+        present(alert, animated: true)
     }
     
 }
