820de12d36a24801533dc8e73ef16f604b0f2dc6
bowski43 <43mngt@gmail.com>
2023-11-24 14:23:28 +0300

    business logic welcome vc

--- AboutMeApp/WelcomeViewController.swift
+++ AboutMeApp/WelcomeViewController.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
-
+final class WelcomeViewController: UIViewController {
+    
+    var welcomeText = ""
+    
+    @IBOutlet private weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        welcomeLabel.text = welcomeText
     }
-
+    
 }
